9ff394eccaad249afe668456d354925929f3b621
Brandon <47222125+BbarrettCFR@users.noreply.github.com>
2019-05-29 16:18:23 -0400

    Update button toggle text
    TODO: Allow for back and forth, rather than one click one change.

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,19 +1,18 @@
 import React, { Component } from "react";
 import HeaderInformation from "./Header/HeaderInformation";
 import "./styles.css";
-
 class AppContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { showing: false };
+    this.state = { showing: false, label: "see more" };
   }
 
   render() {
     const { showing } = this.state;
     return (
       <div>
-        <button onClick={() => this.setState({ showing: !showing })}>
-          see more
+        <button onClick={() => this.setState({ showing: !showing, label: "see less" })}>
+          {this.state.label}
         </button>
         {showing ? (
           <div>
@@ -25,3 +24,4 @@ class AppContainer extends Component {
   }
 }
 export default AppContainer;
+
